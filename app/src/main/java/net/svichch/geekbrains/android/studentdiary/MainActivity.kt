8651d19cb70644620c88bdf08d63da082b8c3781
Vladimir Svishch <5693031@gmail.com>
2021-11-19 12:28:00 +0300

    Initial commit

---
+++ app/src/main/java/net/svichch/geekbrains/android/studentdiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.svichch.geekbrains.android.studentdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
